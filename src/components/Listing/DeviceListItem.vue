2fc639455d469d9c1cb85c9ec301da47ade7d333
Chandra Shekhar <shekharait254@gmail.com>
2019-12-02 19:24:24 +0530

    Add listing for device list

--- src/components/Listing/DeviceListItem.vue
+++ src/components/Listing/DeviceListItem.vue
@@ -2,7 +2,7 @@
     <b-card @click.prevent="toggleItem($event,data.id)" :class="{'d-flex flex-row':true,'active' : selectedItems.includes(data.id)}" no-body>
         <div class="pl-2 d-flex flex-grow-1 min-width-zero">
             <div class="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
-                <router-link :to="`/app/device-configuration/devices/${data.id}`" class="w-10 w-sm-100">
+                <router-link :to="`/app/device-configuration/device/${data.id}`" class="w-10 w-sm-100">
                     <p class="list-item-heading mb-1 truncate">{{data.name}}</p>
                 </router-link>
                 <p class="mb-1 text-muted text-small w-15 w-sm-100">{{data.description}}</p>
