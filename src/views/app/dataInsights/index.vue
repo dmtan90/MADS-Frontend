2bd8669ebe3a29ac7376eba3843507a98f4ca9dd
awesomearpit <jarpit1298@gmail.com>
2020-09-16 15:48:39 +0530

    Add help and home section in data insights

--- src/views/app/dataInsights/index.vue
+++ src/views/app/dataInsights/index.vue
@@ -1,22 +1,30 @@
 <template>
   <app-window :appName="'dataInsights'">
     <template v-slot:content>
+      <div v-if="appVuexState.currentSection === 'home'" class="h-100">
+        <data-insights></data-insights>
+      </div>
       <div v-if="appVuexState.currentSection === 'builders'" class="h-100">
         <div> Builders section </div>
       </div>
       <div v-if="appVuexState.currentSection === 'settings'" class="h-100">
         <div> Settings </div>
       </div>
+      <help :appName="'Data Insights'" v-if="appVuexState.currentSection === 'help'"></help>
     </template>
   </app-window>
 </template>
 
 <script>
 import appWindow from './../appWindow'
+import help from './../shared/help'
+import dataInsights from './dataInsights'
 
 export default {
   components: {
-    appWindow
+    appWindow,
+    help,
+    dataInsights
   },
   data () {
     return {
