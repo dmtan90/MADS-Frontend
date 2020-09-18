2a913c613e3b1156453cbd4af0c6bccf1cc112f8
awesomearpit <jarpit1298@gmail.com>
2020-09-18 12:25:34 +0530

    Update the Settigs tab UI

--- src/views/app/dataInsights/settings/fieldForm.vue
+++ src/views/app/dataInsights/settings/fieldForm.vue
@@ -1,10 +1,10 @@
 <template>
   <b-form class="field-form">
         <b-form-group label="Name" label-for="name">
-            <b-form-input v-model="name" type="text" id="name"></b-form-input>
+            <b-form-input v-model="field.name" type="text" id="name"></b-form-input>
         </b-form-group>
         <b-form-group label="Description" label-for="desc">
-          <b-form-input v-model="desc" type="text" id="desc"></b-form-input>
+          <b-form-input v-model="field.desc" type="text" id="desc"></b-form-input>
         </b-form-group>
         <b-form-checkbox
             v-model="status"
@@ -15,17 +15,21 @@
         Hidden
         </b-form-checkbox>
         <b-form-group label="Field Type" label-for="field-type">
-          <multiselect v-model="fieldType" :options="fieldTypes"></multiselect>
+          <multiselect v-model="field.fieldType" :options="fieldTypes"></multiselect>
         </b-form-group>
         <b-form-group label="Query Type" label-for="query-type">
-          <multiselect v-model="queryType" :options="queryTypes"></multiselect>
+          <multiselect v-model="field.queryType" :options="queryTypes"></multiselect>
         </b-form-group>
         <b-form-group label="Scope Type" label-for="scope-type">
-           <multiselect v-model="scopeType" :options="scopeTypes"></multiselect>
+           <multiselect v-model="field.scopeType" :options="scopeTypes"></multiselect>
         </b-form-group>
         <b-form-group label="Key" label-for="key">
-          <b-form-input v-model="key" type="text" id="key"></b-form-input>
+          <b-form-input v-model="field.key" type="text" id="key"></b-form-input>
         </b-form-group>
+        <div class="button-box">
+            <b-button @click="cancel()">Cancel</b-button>
+            <b-button @click="save()">Save</b-button>
+        </div>
       </b-form>
 </template>
 
@@ -35,24 +39,28 @@ export default {
   components: {
 
   },
+  props: {
+    field: {
+      type: Object
+    }
+  },
   data () {
     return {
-      name: '',
-      desc: '',
       status: '',
       fieldTypes: ['STRING', 'NUMERIC', 'DATE', 'BOOLEAN', 'CALCULATED', 'STATE'],
       queryTypes: [' ENTITY_NAME ', ' ENTITY_LABEL ', ' OWNER ', ' ATTRIBUTE ', ' TELEMETRY '],
-      fieldType: '',
-      queryType: '',
-      type: '',
-      scopeType: '',
       scopeTypes: [' CLIENT_SCOPE ', ' SERVER_SCOPE ', ' SHARED_SCOPE ']
     }
   },
   computed: {
   },
   methods: {
+    cancel () {
 
+    },
+    save () {
+
+    }
   }
 }
 </script>
