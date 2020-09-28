163e4c089a897b5665ab8a68eea67faae5bf9c92
awesomearpit <jarpit1298@gmail.com>
2020-09-28 17:54:13 +0530

    login, signup, forgot password UI

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -3,10 +3,6 @@
     <b-colxx xxs="12" md=12  class="mx-auto auth-main">
       <b-card class="auth-card" no-body>
           <div class="position-relative image-side ">
-            <!-- <p class=" text-white h2">{{ $t('pages.magic-is-in-the-details') }}</p>
-              <p class="white mb-0">Please use your credentials to login.<br>If you are not a member, please
-                <router-link tag="a" to="/user/register" class="white">register</router-link>.
-              </p> -->
               <div class="text-box">
                 <p class="text t1">Welcome to</p>
                 <p class="text t2">MADS!</p>
@@ -34,22 +30,10 @@
               </div>
               <div class="input-box">
                 <label>Password</label>
-                <!-- <div class="input">
-                  <div class="icon"></div>
-                  <input type="password" class="form-control" v-model="password">
-                </div> -->
-                  <input type="password" class="form-control" v-model="password">
-              </div>
-              <!-- <label class="form-group">
-                <input type="email" class="form-control" v-model="email">
-                <span>{{ $t('user.email') }}</span>
-              </label>
-              <label class="form-group has-float-label mb-4">
                 <input type="password" class="form-control" v-model="password">
-                <span>{{ $t('user.password') }}</span>
-              </label> -->
+              </div>
               <div class="button-box">
-                <b-button type="submit">{{ $t('buttons.login')}}</b-button>
+                <b-button type="submit">Sign In</b-button>
               </div>
               <div class="checkbox-box">
                 <b-form-checkbox
@@ -68,11 +52,6 @@
               <div class="forgot-text">
                 Not a member yet? <router-link tag="a" to="/user/register">Register here</router-link>
               </div>
-              
-              <!-- <div class="d-flex justify-content-between align-items-center">
-                  <router-link tag="a" to="/user/forgot-password">{{ $t('user.forgot-password-question')}}</router-link>
-                  <b-button type="submit" variant="primary" size="lg" class="btn-shadow btn-login">{{ $t('buttons.login')}}</b-button>
-              </div> -->
           </b-form>
           <div class="footer-box">
             <ul>
--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -1,5 +1,64 @@
 <template>
-  <b-row class="h-100">
+ <b-row class="main-height">
+    <b-colxx xxs="12" md=12  class="mx-auto auth-main">
+      <b-card class="auth-card" no-body>
+          <div class="position-relative image-side ">
+            <!-- <p class=" text-white h2">{{ $t('pages.magic-is-in-the-details') }}</p>
+              <p class="white mb-0">Please use your credentials to login.<br>If you are not a member, please
+                <router-link tag="a" to="/user/register" class="white">register</router-link>.
+              </p> -->
+              <div class="text-box">
+                <p class="text t1">Welcome to</p>
+                <p class="text t2">MADS!</p>
+                <p class="text t3">
+                  <ul>
+                    <li>Monitor</li>
+                    <li><span></span></li>
+                    <li>Automate</li>
+                    <li><span></span></li>
+                    <li>Diagnose</li>
+                    <li><span></span></li>
+                    <li>Secure</li>
+                  </ul>
+                </p>
+                <p class="text t4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+          </div>
+          <div class="form-side">
+            <router-link tag="a" to="/"><span class="logo-single"/></router-link>
+            <div class="heading-text">Sign Up to MADS!</div>
+            <b-form @submit.prevent="formSubmit">
+              <div class="input-box">
+                <label>Full Name</label>
+                <input type="text" class="form-control" v-model="fullname">
+              </div>
+              <div class="input-box">
+                <label>Email Address</label>
+                <input type="email" class="form-control" v-model="email">
+              </div>
+              <div class="input-box">
+                <label>Password</label>
+                <input type="password" class="form-control" v-model="password">
+              </div>
+              <div class="button-box">
+                <b-button type="submit">Sign Up</b-button>
+              </div>
+              <div class="forgot-text">
+                Already member? <router-link tag="a" to="/user/login">Login</router-link>
+              </div>
+          </b-form>
+          <div class="footer-box">
+            <ul>
+              <li>© 2020, DataKrew, All Rights Reserved</li>
+              <li>Privacy & Terms</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+      </b-card>
+    </b-colxx>
+  </b-row>
+  <!-- <b-row class="h-100">
     <b-colxx xxs="12" md=10  class="mx-auto my-auto">
       <b-card class="auth-card" no-body>
           <div class="position-relative image-side ">
@@ -31,7 +90,7 @@
         </div>
       </b-card>
     </b-colxx>
-  </b-row>
+  </b-row> -->
 </template>
 <script>
 export default {
